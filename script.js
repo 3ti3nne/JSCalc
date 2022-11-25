d0e6489a9831069a9445a66bff7a6bf1fc0a9c3a
Etienne ROUBAUD <eti3nne.roubaud@gmail.com>
2022-11-25 17:38:01 +0100

    feat: calculate allright.
    missing multiple operators calculation
    
    will come back later

--- script.js
+++ script.js
@@ -7,10 +7,79 @@ const d = document;
 const equalBtn = d.querySelector("[data-equal]");
 const operationBtns = d.querySelectorAll("[data-operation]");
 const numberBtns = d.querySelectorAll("[data-number]");
-const outputScreen = d.querySelector(".outputText");
+let outputScreen = d.querySelector(".outputText");
+const clearBtn = d.querySelector(".clear");
+const virgule = d.querySelector(".virgule");
+
+let firstNumber = "";
+let secondNumber = "";
+let operator = "";
+
+clearBtn.addEventListener("click", () => {
+  outputScreen.innerHTML = "";
+  firstNumber = "";
+  secondNumber = "";
+  operator = "";
+});
 
 numberBtns.forEach((btn) =>
   btn.addEventListener("click", () => {
     outputScreen.innerHTML += btn.innerHTML;
   })
 );
+
+operationBtns.forEach((btn) =>
+  btn.addEventListener("click", () => {
+    outputScreen.innerHTML += btn.innerHTML;
+  })
+);
+
+function calculate(a, searchingOperator, b) {
+  a = parseInt(a);
+  b = parseInt(b);
+  switch (searchingOperator) {
+    case "+":
+      return (a + b).toString();
+      break;
+    case "*":
+      return (a * b).toString();
+      break;
+    case "/":
+      return (a / b).toString();
+      break;
+    case "-":
+      return (a - b).toString();
+      break;
+  }
+}
+
+equalBtn.addEventListener("click", () => {
+  let myString = "";
+
+  myString = outputScreen.firstChild.data;
+
+  let theIndex = [...myString].findIndex((searchingOperator) => {
+    if (
+      searchingOperator === "-" ||
+      searchingOperator === "*" ||
+      searchingOperator === "+" ||
+      searchingOperator === "/"
+    ) {
+      return true;
+    }
+  });
+  for (let i = 0; i < theIndex; i++) {
+    firstNumber += [...myString][i];
+  }
+  for (let i = theIndex + 1; i < [...myString].length; i++) {
+    secondNumber += [...myString][i];
+  }
+  operator = [...myString][theIndex];
+
+  outputScreen.innerHTML = calculate(firstNumber, operator, secondNumber);
+
+  firstNumber = "";
+  secondNumber = "";
+  console.log(firstNumber);
+  console.log(secondNumber);
+});
